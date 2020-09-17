66d82a91a49304db6289d33380d437e4c830669c
DrMurgz <44333861+DrMurgz@users.noreply.github.com>
2020-09-17 12:11:18 +0300

    Initial commit

---
+++ RealEstate-Bot-master/README.md
@@ -0,0 +1,5 @@
+# RealEstate-Bot
+A ChatBot for real estate based on Swift and Watson services
+
+
+
---
+++ RealEstate-Bot-master/Real Estate/Credentials.swift
@@ -0,0 +1,11 @@
+public struct Credentials {
+    
+    public static let ConversationUsername = “<conversation username>"
+    public static let ConversationPassword = “<conversation password>"
+    public static let ConversationWorkspace = “<conversation workspace id>"
+    public static let SpeechToTextUsername = “<speech to text username>”
+    public static let SpeechToTextPassword = “<speech to text password>"
+    public static let TextToSpeechUsername = “<text to speech username>"
+    public static let TextToSpeechPassword = “<text to speech password>"
+
+}
